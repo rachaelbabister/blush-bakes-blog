41176c6e9551648f1fd18d4e8f0a1c153d664adc
rachaelbabister <rachaelbabister@gmail.com>
2024-04-18 18:09:37 +0000

    Add secret key to env.py file and update settings file

--- blushbakes/settings.py
+++ blushbakes/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2h99mvc#t4_pe^67r3n(#@562w&c$3o@k_gklp%d!o-0=xpi69'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
