3a2510e79f61b6473717feb2c4e34afb440cca9e
rachaelbabister <rachaelbabister@gmail.com>
2024-04-18 17:28:08 +0000

    Organise folders into a better structure and amend file paths accordingly

--- blushbakes/urls.py
+++ blushbakes/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog.views import my_blog
+from apps.blog.views import my_blog
 
 urlpatterns = [
-    path('blog/', my_blog, name='blog'),
+    path('apps/blog/', my_blog, name='blog'),
     path('admin/', admin.site.urls),
 ]
