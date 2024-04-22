925ac1809caafda6847c4c04dd3c5f71840eda47
rachaelbabister <rachaelbabister@gmail.com>
2024-04-22 14:33:32 +0000

    Install summernote and create admin decorator for clarity on admin panel

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Recipe
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
 
 # Register your models here.
 
-admin.site.register(Recipe)
