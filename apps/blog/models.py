16affabe218c0a8ae846b07b833f8a9ce59790e1
rachaelbabister <rachaelbabister@gmail.com>
2024-04-22 09:03:08 +0000

    Create Recipe model for blog

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=205, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    # featured_image = CloudinaryField('image', default='placeholder') ** commented out until Cloudinary is installed
+    content = models.TextField()
+    difficulty_rating = models.CharField(max_length=50)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
