374eefa878ca94d0f67877ce83cddd3ff1496da5
rachaelbabister <rachaelbabister@gmail.com>
2024-04-23 16:35:33 +0000

    Amend base.html template with bootstrap coding and add static image files

--- blushbakes/settings.py
+++ blushbakes/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIR = [os.path.join(BASE_DIR, 'static'),]
 
 # Default primary key field type
