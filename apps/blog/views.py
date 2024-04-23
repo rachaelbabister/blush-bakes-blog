4122c1ef805c575625931c1e920c2d597128ef58
rachaelbabister <rachaelbabister@gmail.com>
2024-04-23 15:24:13 +0000

    Add recipes using a JSON Fixture. 'created_on' was removed for this, and then readded after migration took place

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -6,4 +6,4 @@ from .models import Recipe
 class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1)
     template_name = "blog/index.html"
-    paginate_by = 8
+    paginate_by = 4
