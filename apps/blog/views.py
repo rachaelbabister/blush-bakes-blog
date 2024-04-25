4f9fba234f5463775eb7f6171a43099f3821ed4a
rachaelbabister <rachaelbabister@gmail.com>
2024-04-25 14:56:59 +0000

    Add recipe_detail html page and view

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Recipe
 
@@ -6,4 +6,28 @@ from .models import Recipe
 class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1)
     template_name = "blog/index.html"
-    paginate_by = 9
+    paginate_by = 6
+
+
+def recipe_detail(request, slug):
+    """
+    Display an individual :model:`blog.Recipe`.
+
+    **Context**
+
+    ``recipe``
+        An instance of :model:`blog.Recipe`.
+
+    **Template:**
+
+    :template:`blog/recipe_detail.html`
+    """
+
+    queryset = Recipe.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/recipe_detail.html",
+        {"post": recipe},
+    )
