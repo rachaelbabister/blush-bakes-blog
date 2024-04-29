1c869328f8e6f8816285c30beb106e0d39c2f2ea
rachaelbabister <rachaelbabister@gmail.com>
2024-04-29 21:09:46 +0000

    Add ability to display and create new comments to recipe posts

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Recipe
+from .forms import CommentForm
+
 
 # Create your views here.
 class RecipeList(generic.ListView):
@@ -25,9 +28,31 @@ def recipe_detail(request, slug):
 
     queryset = Recipe.objects.filter(status=1)
     recipe = get_object_or_404(queryset, slug=slug)
+    comments = recipe.comments.all().order_by("-created_on")
+    comment_count = recipe.comments.filter(approved=True).count()
+    recipe.comments.filter(approved=True).count()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.recipe = recipe
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Thank you, your comment will appear once approved.'
+            )
+
+    comment_form = CommentForm()
 
     return render(
         request,
         "blog/recipe_detail.html",
-        {"recipe": recipe},
+        {
+            "recipe": recipe,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form,
+        },
     )
