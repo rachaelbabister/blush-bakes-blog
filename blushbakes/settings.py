ee7ba80c2c48d6bf10b71161d06f19107878be1b
rachaelbabister <rachaelbabister@gmail.com>
2024-05-03 00:23:24 +0000

    Change Database URL back to postgres and not local sqlite

--- blushbakes/settings.py
+++ blushbakes/settings.py
@@ -97,17 +97,9 @@ WSGI_APPLICATION = 'blushbakes.wsgi.application'
 
 
 # Database
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 CSRF_TRUSTED_ORIGINS = [
